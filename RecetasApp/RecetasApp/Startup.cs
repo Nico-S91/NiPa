e83ac775df1bf3626520ca9ab51abfa2215d48fe
Nicolas Sibikowski <niko.sibi@gmail.com>
2016-10-14 18:57:22 -0300

    Subo los primeros archivos base del proyecto.

---
+++ RecetasApp/RecetasApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecetasApp.Startup))]
+namespace RecetasApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
